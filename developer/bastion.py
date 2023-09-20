904c8ebfed8d99fda6cf7cf726dc63b9baa04194
Jason Wilcox <jason@jasonsays.com>
2023-09-20 11:12:55 -0600

    geneate keypair

--- developer/bastion.py
+++ developer/bastion.py
@@ -2,15 +2,43 @@
 import pulumi_awsx as awsx
 import pulumi
 from vpc import mtn_vpc, mtn_public_subnets, mtn_eks_cp_subnets
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
 
 
 # Get the stack name for generating the key pair name
 stack_name = pulumi.get_stack()
 
-# Create an AWS key pair resource
-key_pair = aws.ec2.KeyPair("my-key-pair",
-    key_name=stack_name,  # Use stack name as the key name
+
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
 )
+
+private_key_pass = b"your-password"
+
+encrypted_pem_private_key = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.BestAvailableEncryption(private_key_pass)
+)
+
+pem_public_key = private_key.public_key().public_bytes(
+  encoding=serialization.Encoding.PEM,
+  format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+private_key_file = open(stack_name + ".pem", "w")
+private_key_file.write(encrypted_pem_private_key.decode())
+private_key_file.close()
+
+public_key_file = open(stack_name + ".pub", "w")
+public_key_file.write(pem_public_key.decode())
+public_key_file.close()
+
+# Create an AWS key pair resource
+key_pair = aws.ec2.KeyPair( key_name=stack_name, public_key=pem_public_key.decode() ) # Use stack name as the key name
 
 # Export the public key for the key pair
 pulumi.export("public_key", key_pair.key_name)
